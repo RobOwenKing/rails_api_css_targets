d1e750999aff028b08c6b6ede347beb4f6647a78
Robert King <robert.o.king@hotmail.co.uk>
2021-08-19 10:41:01 +0200

    Added: Challenges show request tests

--- spec/requests/challenges_spec.rb
+++ spec/requests/challenges_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Challenges', type: :request do
   let(:challenge_id) { challenges.first.id }
 
   # Tests for :index
-  describe 'GET /index' do
+  describe 'index request' do
     before { get '/challenges' }
 
     it 'returns JSON' do
@@ -20,4 +20,26 @@ RSpec.describe 'Challenges', type: :request do
   end
 
   # Tests for :show
+  describe 'show request' do
+    before { get "/challenges/#{challenge_id}" }
+
+    context 'when the record exists' do
+      it 'returns the challenge' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(challenge_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:challenge_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
