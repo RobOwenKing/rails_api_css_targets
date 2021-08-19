bdcb2e93f93aa90d5daa7fb9a69bcd3a45f6cbce
Robert King <robert.o.king@hotmail.co.uk>
2021-08-19 10:31:02 +0200

    Added: Challenges controller index and show methods

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,2 +1,11 @@
 class ChallengesController < ApplicationController
+  def index
+    @challenges = Challenge.all
+    render json: @challenges
+  end
+
+  def show
+    @challenge = Challenge.find(params[:id])
+    render json: @challenge
+  end
 end
